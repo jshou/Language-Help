83818ecf97cba765f7ca3836f995a7cc49177c43
Joshua Hou <joshuahou@gmail.com>
2011-02-14 23:40:49 -0800

    removed unused overlay, increased version number

--- chrome/content/overlay.js
+++
@@ -1,9 +0,0 @@
-var languagehelp = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.strings = document.getElementById("languagehelp-strings");
-  },
-};
-
-window.addEventListener("load", languagehelp.onLoad, false);
